341d2edd6f4aeefa161363e4d9f9a6759549402d
Marília <mariliamarquesgus@gmail.com>
2025-06-17 00:12:40 -0300

    update to render

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -9,13 +9,5 @@ if (process.env.NODE_ENV === 'production') {
   pg.types.setTypeParser(20, 'text', parseInt);
 }
 
-const getEnvironment = () => {
-  switch (process.env.NODE_ENV) {
-    case 'production': return production;
-    case 'test': return test;
 
-    default: return development;
-  }
-}
-
-export const Knex = knex(getEnvironment());
+export const Knex = knex(production);
