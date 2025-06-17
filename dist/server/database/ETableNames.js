261e5921062619126ca1fa0a569ed9ee2083c1ea
Marília <120988790+mariliamarquess@users.noreply.github.com>
2025-06-17 17:15:56 -0300

    Delete dist directory

--- dist/server/database/ETableNames.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ETableNames = void 0;
-var ETableNames;
-(function (ETableNames) {
-    ETableNames["cidade"] = "cidade";
-    ETableNames["pessoa"] = "pessoa";
-    ETableNames["usuario"] = "usuario";
-})(ETableNames || (exports.ETableNames = ETableNames = {}));
